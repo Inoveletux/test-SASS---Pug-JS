a64ecfeeb0c7b64652fc41bc473dabe2060efe06
Théau Méry <theaumery@gmail.com>
2021-03-31 12:13:37 +0200

    🚚MOV: Changement du système de routes et passage des variables en JSON

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+let fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+fs.readFile( 'views/includes/vars.json', 'utf8', 
+    (err, dataEnString) => {
+        if (err)
+            throw err;
+        console.log(dataEnString)
+        let dataEnObjet = JSON.parse(dataEnString)
+        console.log(dataEnObjet)
+      
+        
+        router.get('/', function(req, res, next) {
+        res.render('index', { title: 'Express', monMenu:dataEnObjet });
 });
+    }
+);
+
+
 
 module.exports = router;
